9e5b6534b1cb28205db7152da2dcd45d7937fea2
Stefan Wahren <stefan.wahren@chargebyte.com>
2025-10-20 16:25:01 +0200

    EvseV2G: Allow Fake DC (DIN70121 on AC charger)
    Allow the popular DIN70121 on AC charger to handle fake DC.
    
    Signed-off-by: Stefan Wahren <stefan.wahren@chargebyte.com>

--- modules/EVSE/EvseV2G/charger/ISO15118_chargerImpl.cpp
+++ modules/EVSE/EvseV2G/charger/ISO15118_chargerImpl.cpp
@@ -336,11 +336,6 @@ void ISO15118_chargerImpl::handle_update_energy_transfer_modes(
                 break;
             }
         }
-
-        if (mod->config.supported_DIN70121 == true and v2g_ctx->is_dc_charger == false) {
-            v2g_ctx->supported_protocols &= ~(1 << V2G_PROTO_DIN70121);
-            dlog(DLOG_LEVEL_WARNING, "Removed DIN70121 from the list of supported protocols as AC is enabled");
-        }
     }
 }
 
