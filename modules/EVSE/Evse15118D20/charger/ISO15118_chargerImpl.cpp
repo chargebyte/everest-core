bd26694650a4ea26df1b6330566bdf848fe65f50
Martin Lukas <martin.lukas@chargebyte.com>
2026-01-22 10:02:04 +0100

    EvseIso15118D20 and types/iso15118.yaml: Add missing MCS to ServiceCategory and connector entry
    Signed-off-by: Martin Lukas <martin.lukas@chargebyte.com>

--- modules/EVSE/Evse15118D20/charger/ISO15118_chargerImpl.cpp
+++ modules/EVSE/Evse15118D20/charger/ISO15118_chargerImpl.cpp
@@ -549,6 +549,10 @@ iso15118::session::feedback::Callbacks ISO15118_chargerImpl::create_callbacks() 
             selected_parameters.energy_transfer = types::iso15118::ServiceCategory::DC_BPT;
         } else if (parameters.selected_energy_service == dt::ServiceCategory::DC_ACDP_BPT) {
             selected_parameters.energy_transfer = types::iso15118::ServiceCategory::DC_ACDP_BPT;
+        } else if (parameters.selected_energy_service == dt::ServiceCategory::MCS) {
+            selected_parameters.energy_transfer = types::iso15118::ServiceCategory::MCS;
+        } else if (parameters.selected_energy_service == dt::ServiceCategory::MCS_BPT) {
+            selected_parameters.energy_transfer = types::iso15118::ServiceCategory::MCS_BPT;
         } else {
             EVLOG_critical << "Energy service is apparently no energy service!";
         }
@@ -566,6 +570,24 @@ iso15118::session::feedback::Callbacks ISO15118_chargerImpl::create_callbacks() 
                 selected_parameters.connector = types::iso15118::Connector::Extended;
             } else if (*dc_connector == dt::DcConnector::Dual2) {
                 selected_parameters.connector = types::iso15118::Connector::Dual2;
+            } else {
+                selected_parameters.connector = types::iso15118::Connector::Dual4;
+            }
+        } else if (const auto* mcs_connector = std::get_if<dt::McsConnector>(&parameters.selected_connector)) {
+            if (*mcs_connector == dt::McsConnector::Mcs) {
+                selected_parameters.connector = types::iso15118::Connector::Mcs;
+            } else if (*mcs_connector == dt::McsConnector::Chaoji) {
+                selected_parameters.connector = types::iso15118::Connector::Chaoji;
+            } else if (*mcs_connector == dt::McsConnector::UltraChaoji) {
+                selected_parameters.connector = types::iso15118::Connector::UltraChaoji;
+            } else if (*mcs_connector == dt::McsConnector::rMcs) {
+                selected_parameters.connector = types::iso15118::Connector::rMcs;
+            } else if (*mcs_connector == dt::McsConnector::xMcs) {
+                selected_parameters.connector = types::iso15118::Connector::xMcs;
+            } else if (*mcs_connector == dt::McsConnector::Aviation) {
+                selected_parameters.connector = types::iso15118::Connector::Aviation;
+            } else if (*mcs_connector == dt::McsConnector::Marine) {
+                selected_parameters.connector = types::iso15118::Connector::Marine;
             } else {
                 selected_parameters.connector = types::iso15118::Connector::Dual4;
             }
